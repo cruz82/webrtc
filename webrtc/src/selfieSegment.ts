83b4a6104c26f99dd283aaf0841be613ad70ead1
Lee Lian Hoy <cruz@zyk3.net>
2024-11-19 11:56:17 +0900

    cleanup

--- webrtc/src/selfieSegment.ts
+++ webrtc/src/selfieSegment.ts
@@ -1,5 +1,5 @@
 import { SelfieSegmentation } from "@mediapipe/selfie_segmentation";
-import { fpsToInterval } from "./utilities";
+import { calculateScaling, fpsToInterval } from "./utilities";
 
 export async function segmentVideo(videoElement: HTMLVideoElement) {
   const canvas = document.createElement("canvas");
@@ -10,6 +10,21 @@ export async function segmentVideo(videoElement: HTMLVideoElement) {
     throw new Error(`Canvas 2d context is not available: ${canvas}`);
   }
 
+  let bg: ImageBitmap;
+  const image = new Image();
+  image.onload = () => {
+    const { dX, dY, width, height } = calculateScaling(
+      image.width,
+      image.height,
+      canvas.width,
+      canvas.height
+    );
+    createImageBitmap(image, dX, dY, width, height).then((bitmap) => {
+      bg = bitmap;
+    });
+  };
+  image.src = "/assets/sample_menu.png";
+
   const segment = new SelfieSegmentation({
     locateFile: (file) =>
       `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation@0.1/${file}`,
@@ -18,11 +33,17 @@ export async function segmentVideo(videoElement: HTMLVideoElement) {
   segment.setOptions({ modelSelection: 0, selfieMode: true });
   segment.onResults((result) => {
     ctx.save();
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(result.segmentationMask, 0, 0, canvas.width, canvas.height);
 
-    ctx.globalCompositeOperation = "source-in";
+    ctx.globalCompositeOperation = "source-out";
+    ctx.fillStyle = "#0000FF7F";
+    ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
+
+    ctx.globalCompositeOperation = "destination-atop";
     ctx.drawImage(result.image, 0, 0, canvas.width, canvas.height);
+
     ctx.restore();
   });
 
